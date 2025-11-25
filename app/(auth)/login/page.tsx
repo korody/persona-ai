fd02780741fed8547584bccead994bc820f13d6f
Marcos Korody <marko@persona.cx>
2025-11-25 00:34:35 -0300

    fix: Import useEffect in login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@
 
 'use client'
 
-import { Suspense, useState } from 'react'
+import { Suspense, useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
